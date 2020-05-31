f1de7d740d1cf60b8e292a9b48882c98b020168e
Nathan Hines <nhinesathome@hotmail.com>
2020-05-31 13:52:39 -0600

    Start adding Modal

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
-    
+    $(function() {
+        // this initializes the dialog 
+        $("#dialog").dialog({
+          autoOpen : false, modal : true
+        });
+      
+        // the onclick handler
+        $("#newReference").click(function() {
+          $("#dialog").dialog("open");
+          return false;
+        });
+      });
   
   });
 
