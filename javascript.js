d00cbfbd07d88b7680520d214d7ec07c8cb24926
Nathan Hines <nhinesathome@hotmail.com>
2020-06-02 07:56:17 -0600

    JQuery changes to dialog box

--- javascript.js
+++ javascript.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
+  
+  var firstname = $("#firstname");
+  var lastname = $("#lastname");
 
-    $(function() {
-        // this initializes the dialog 
-        $("#dialog").dialog({
-          autoOpen : false, modal : true
+
+    $(function openModal() {  
+
+      // this initializes the dialog 
+      $("#dialog").dialog({
+      autoOpen : false, modal : true
         });
-      
-        // the onclick handler
-        $("#newReference").click(function() {
-          $("#dialog").dialog("open");
-          return false;
+
+      // the onclick handler
+      $("#newReference").click(function() {
+        $("#dialog").dialog("open");
+        return false;
         });
+
       });
+    
   
   });
 
