2e3dccec38020d346f058cc728c9b9f04f4a8914
nathanhines <45949540+nathanhines@users.noreply.github.com>
2020-06-11 14:46:36 -0600

    Add Dialog box (#4)
    * Head Notes
    
    * Start adding Modal
    
    * JQuery changes to dialog box
    
    * Script work
    
    * Finish Dialog box and validation
    
    * Finshed Dialog Box Form
    
    * DS-store
    
    * Javascript

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+
+
+$(document).ready(function(){ 
 
   $( function() {
     $( "#accordion" ).accordion({
@@ -6,7 +8,38 @@ $(document).ready(function(){
     });
   } );
   
+var $dialog = $("#dialog");
+      // this initializes the dialog 
+      $dialog.dialog({
+      autoOpen : false, modal : true, height : 500
+        });
+
+      // the onclick handler
+      $("#newReference").click(function() {
+        $dialog.dialog("open");
+        $(".validateTips").html("All form fields are required.")
+        return false;
+        });
+
+    
+      $("#reference-form").submit(function(){
+        var firstname = document.getElementById("firstname").value;
+        var lastname = document.getElementById("lastname").value;
+
+        if (firstname == "") {
+          $(".validateTips").html("Error: No input")
+          return false
+        } else if (lastname == "") {
+          $(".validateTips").html("Error: No input")
+          return false
+        } else {
+          $('.list-group').append("<li class='list-group-item ppl-know-person'><img src='images/blank-profile-picture.png' /><p>"+firstname+" "+lastname+"</p></li>");
+
+          $dialog.dialog("close");
+          return false
+
+        };
+
+      });
+  
   });
-
-
-
